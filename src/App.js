fd78296699dd452b146f13ea0d3a466864324871
Adão Benites <adaobjr@gmail.com>
2021-07-30 08:47:39 -0400

    feat: :sparkles: added header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [showMenu, setShowMenu] = useState(false);
+  // ---------------------------------------------------------------------------------------------
+    // CICLOS DE VIDA
+    // useEffect(() => { Aos.init({ duration: 2000 }); }, []);
+
+    // ---------------------------------------------------------------------------------------------
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       {/* <!--===== HEADER =====--> */}
+    <header className="header">
+        <nav className="nav layoutGrid">
+            <div>
+                <a href="#home" className="navLogo">Adão Benites</a>
+            </div>
+            <div className={ (showMenu) ? 'navMenu show' : 'navMenu' }>
+                <ul className="navList" onClick={ () => setShowMenu(!showMenu) }>
+                    <li className="navItem"><a href="#home" className="navLink">Home</a></li>
+                    <li className="navItem"><a href="#about" className="navLink">About</a></li>
+                    <li className="navItem"><a href="#skills" className="navLink">Skills</a></li>
+                    <li className="navItem"><a href="#portfolio" className="navLink">Portfolio</a></li>
+                    <li className="navItem"><a href="#contact" className="navLink">Contact</a></li>
+                </ul>
+            </div>
+            <button
+                type="button"
+                onClick={ () => setShowMenu(!showMenu) }
+                className={ (showMenu) ? 'icon iconActive' : 'icon' }>
+                <div className="hamburguer" />
+            </button>
+        </nav>
+    </header>
     </div>
   );
 }
